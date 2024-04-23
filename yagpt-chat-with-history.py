07c5f576cc546a65df25c5c95042369be749dcf2
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2024-04-23 08:45:56 +0300

    2nd commit

--- yagpt-chat-with-history.py
+++ yagpt-chat-with-history.py
@@ -116,7 +116,7 @@ def main():
 
 
     yagpt_temperature = st.sidebar.slider("Степень креативности (температура)", 0.0, 1.0, 0.3)
-    yagpt_max_tokens = st.sidebar.slider("Размер контекстного окна (в [токенах](https://cloud.yandex.ru/ru/docs/yandexgpt/concepts/tokens))", 200, 8000, 5000)
+    yagpt_max_tokens = st.sidebar.slider("Размер контекстного окна (в [токенах](https://cloud.yandex.ru/ru/docs/yandexgpt/concepts/tokens))", 200, 8000, 8000)
 
     def history_reset_function():
         # Code to be executed when the reset button is clicked
