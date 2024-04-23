ae6fa29a25036cf57076af1c273a3d977c0539bc
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2024-04-23 20:11:12 +0300

    Voice commit 5

--- yagpt-chat-with-history.py
+++ yagpt-chat-with-history.py
@@ -16,7 +16,7 @@ def ozuvi4it_mp3_fa4il(fa4il_put,  text, api_key):
     # The response is a stream of bytes, so you can write it to a file
     with open(fa4il_put, "wb") as f:
         f.write(res_tts.content)  
-    st.audio(fa4il_put, format="audio/mpeg", loop=True)
+    st.audio(fa4il_put, format="audio/mpeg", loop=False)
 
 
 
@@ -174,7 +174,8 @@ def main():
         response = chain_with_history.invoke({"question": prompt}, config)
         st.chat_message("ai").write(response.content)
         st.button('Озвучить ответ!', on_click=ozuvi4it_mp3_fa4il, args=('./Hello.mp3', response.content, sk_api_ep))
-
+        if st.button:
+            st.audio("./Hello.mp3", format="audio/mpeg", loop=False)
 
     # Отобразить сообщения в конце, чтобы вновь сгенерированные отображались сразу
     with view_messages:
